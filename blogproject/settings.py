2b441c4f9decc8b8fd113406d2590f25c24af08c
何瑛磊 <13738849868@163.com>
2017-11-28 13:29:48 +0800

    update

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^ufwrsq+tqk#dpnk7))j2is%&)wr@3)=tp)w6u=p92y=9w9q8c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ','119.23.53.164']
 
 
 # Application definition
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+# 加入下面的配置
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
